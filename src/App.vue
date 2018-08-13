71819104db598120a831f1c53e03d26a6b70cb3d
Alejandro Steinmetz <asteinmetz78@gmail.com>
2018-08-13 19:56:11 -0300

    Refactor app & add favicon

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h4>{{ appName }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      appName: 'A Parcel & Vue test project.'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
