7c7a5fe599701584eb5ce2a7a8d3a7af947c16b5
Alejandro Steinmetz <asteinmetz78@gmail.com>
2018-08-13 20:36:07 -0300

    Add eslint config
    Add a CoolButton Vue component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
   <div id="app">
     <h4>{{ appName }}</h4>
+    <cool-button text="Button #1"></cool-button>
+    <cool-button text="Button #2"></cool-button>
   </div>
 </template>
 
 <script>
+import CoolButton from "./CoolButton.vue";
+
 export default {
-  name: 'app',
+  name: "app",
   data() {
     return {
-      appName: 'A Parcel & Vue test project.'
-    }
+      appName: "A Parcel & Vue test project."
+    };
+  },
+  components: {
+    CoolButton
   }
-}
+};
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+h4 {
+  background-color: rgb(79, 77, 235);
+  color: white;
+  padding: 3px;
+}
 </style>
-
-
---
+++ src/CoolButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <button @click="btnClick">{{ text }}</button>
+  </div>
+</template>
+<script>
+export default {
+  props: ["text"],
+  data: () => {
+    return {
+
+    };
+  },
+  methods: {
+    btnClick: () => {
+      console.log("Yep! you clicked the button!");
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+</style>
+
+
